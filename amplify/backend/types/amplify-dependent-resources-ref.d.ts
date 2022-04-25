244c32782a2777cf7849ab625a8d6984037e61a2
nmm1049 <Nick.Moreschi@unh.edu>
2022-04-24 21:41:04 -0400

    tutorial finished

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifyapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s3e9e92b70": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
